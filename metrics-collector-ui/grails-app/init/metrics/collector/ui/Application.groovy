42802e7818110ce08f6957ce273ec06d491c382e
Nicolas Doroskevich <nikodc2@gmail.com>
2015-11-28 18:52:35 -0300

    Initial metrics-collector-ui commit. Just an empty Grails webapp.

---
+++ metrics-collector-ui/grails-app/init/metrics/collector/ui/Application.groovy
@@ -0,0 +1,10 @@
+package metrics.collector.ui
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
